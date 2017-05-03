1e5778626260b1d1f80dea1053e61e1097138a71
Rahman <auliar68@gmail.com>
2017-05-03 13:46:37 +0700

    Penambahan Kelas MainMenu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -13,18 +13,28 @@ public class MainMenu {
         //inisialisasi report
         Report report = new Report();
         report.isiPegawai();
-
+//        report.printPegawai();
+//        report.printTransasksi();
         //print daftar barang
 //        barangPajang.print();
-        //menambah customer
-        RegCustomer rahman = new RegCustomer(1,"Rahman","Cimohay",false);
-        MemCustomer benrigo = new MemCustomer(1,"Benrigo","Bubat Bersatu",true);
 
         //Rahman Cart
         Transaksi t1 = new Transaksi();
-        Cart c1 = new Cart(1,2,barangPajang.cariBarang(2));t1.addCart(c1);
+        //menambah customer
+        RegCustomer rahman = new RegCustomer(1,"Rahman","Cimohay",false);t1.setCustomer(rahman);
+        Cart Rah_c1 = new Cart(1,2,barangPajang.cariBarang(2));t1.addCart(Rah_c1);
+        Cart Rah_c2 = new Cart(2,2,barangPajang.cariBarang(1));t1.addCart(Rah_c2);
 
         //cetak struk
-        t1.struk();
+//        t1.struk();
+
+        Transaksi t2 = new Transaksi();
+        //menambah customer
+        MemCustomer benrigo = new MemCustomer(1,"Benrigo","Bubat Bersatu",true);t2.setCustomer(benrigo);
+        Cart Ben_c1 = new Cart(1,2,barangPajang.cariBarang(2));t1.addCart(Ben_c1);
+        Cart Ben_c2 = new Cart(2,2,barangPajang.cariBarang(1));t1.addCart(Ben_c2);
+
+        //cetak struk
+//        t2.struk();
     }
 }
